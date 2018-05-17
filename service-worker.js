f9644595cb8c72a46c9b5450ce97dec7a8a540f0
Benny Powers <benny.p@blink.gg>
2018-05-17 10:29:45 +0300

    Refactors to remove dependency on Polymer.
    Adds some lazy-loaded lit-html for kicks.

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,44 @@
-import { Element } from '../../@polymer/polymer/polymer-element.js';
+let snackbar;
+
+const MDL_SCRIPT_SRC = 'https://code.getmdl.io/1.3.0/material.min.js';
+const MDL_STYLESHEET_HREF = 'https://code.getmdl.io/1.3.0/material.indigo-pink.min.css';
+const MATERIAL_ICON_FONT_HREF = 'https://fonts.googleapis.com/icon?family=Material+Icons';
+
+const isMdlStylesheetLink = link =>
+  link.href === MDL_STYLESHEET_HREF;
+
+const isMaterialIconFontLink = link =>
+  link.href === MATERIAL_ICON_FONT_HREF;
+
+const loadStylesheet = href => new Promise((resolve, reject) => {
+  const link = document.createElement('link');
+        link.href = href;
+        link.onload = resolve;
+        link.onerror = reject;
+  document.head.appendChild(link);
+});
+
+const loadMdlStylesheet = loadStylesheet(MDL_STYLESHEET_HREF);
+const loadMaterialIconFont = loadStylesheet(MATERIAL_ICON_FONT_HREF);
+
+const loadAsync = src => new Promise( async (resolve, reject) => {
+  const stylesheets = [...document.head.querySelectorAll('link[rel="stylesheet"]')];
+  const stylesLoaded = stylesheets.some(isMdlStylesheetLink)
+  const fontLoaded = stylesheets.some(isMaterialIconFontLink)
+  if (!stylesLoaded) await loadMdlStylesheet();
+  if (!fontsLoaded) await loadMaterialIconFont();
+  if ('componentHandler' in window) resolve();
+  const script = document.createElement('script');
+        script.async = true;
+        script.defer = true;
+        script.onload = resolve;
+        script.onerror = reject;
+        script.src = src;
+  document.head.appendChild(script)
+});
 
 if ('serviceWorker' in navigator) {
+
   /**
    * Custom Element for declaratively adding a service worker with
    * "Click To Update" prompt and optional auto-update.
@@ -17,97 +55,96 @@ if ('serviceWorker' in navigator) {
    * --------- | ----------- | ---------
    * `--service-worker-button-color` | Color of the "Click to Update" button text | var(--paper-yellow-400).
    *
-   * @polymer
    * @customElement
+   * @extends HTMLElement
    *
    * @demo demo/index.html
    */
-    class ServiceWorker extends Element {
-      static get is() {return 'service-worker';}
+    class ServiceWorker extends HTMLElement {
+      static get is() {return ;}
 
-      static get properties() {
-        return {
+      static get observedAttributes() {
+        return [
+          'auto-reload',
+          'language',
+          'path',
+          'scope',
+          'toast-duration',
+          'update-action',
+        ];
+      }
 
-          /**
-           * If true, when updates are found, the page will automatically
-           * reload, so long as the user has not yet interacted with it.
-           */
-          autoReload: {
-            type: Boolean,
-            value: false,
+      constructor() {
+        super();
+
+        /**
+         * If true, when updates are found, the page will automatically
+         * reload, so long as the user has not yet interacted with it.
+         */
+        this.autoReload = false;
+
+        /**
+         * language for strings.
+         * e.g. 'he'. defaults to first 2 chars of document lang or 'en'.
+         */
+        this.language = document.documentElement.lang.substring(0, 2) || 'en';
+
+        /** A reference to the service worker instance. */
+        this.worker = null;
+
+        /**
+         * String passed to serviceWorker which triggers self.skipWaiting().
+         * String will be passed in message.action.
+         */
+        this.updateAction = 'skipWaiting';
+
+        /** Reference to the toast element. */
+        this.toast = null;
+
+        /** Duration of the toast. */
+        this.toastDuration = 10000,
+
+        /** Scope for the service worker. */
+        this.scope = '/';
+
+        /** Path to the service worker script. */
+        this.path = '/service-worker.js';
+
+        /** i18n strings. */
+        this.resources = {
+
+          en: {
+            newVersion: 'New Version Ready',
+            ok: 'OK',
+            clickToUpdate: 'Click to Update',
           },
 
-          /**
-           * language for strings.
-           * e.g. 'he'. defaults to first 2 chars of document lang or 'en'.
-           */
-          language: {
-            type: String,
-            value: document.documentElement.lang.substring(0, 2) || 'en',
+          he: {
+            newVersion: 'גרסה חדשה',
+            ok: 'בסדר',
+            clickToUpdate: 'לחץ לעדכן',
           },
 
-          /** Path to the service worker script. */
-          path: {
-            type: String,
-            value: '/service-worker.js',
-          },
-
-          /** i18n strings. */
-          resources: {
-            type: Object,
-            value: () => ({
-
-              en: {
-                newVersion: 'New Version Ready',
-                ok: 'OK',
-                clickToUpdate: 'Click to Update',
-              },
-
-              he: {
-                newVersion: 'גרסה חדשה',
-                ok: 'בסדר',
-                clickToUpdate: 'לחץ לעדכן',
-              },
-
-            }),
-          },
-
-          /** Scope for the service worker. */
-          scope: {
-            type: String,
-            value: '/',
-          },
-
-          /** Duration of the toast. */
-          toastDuration: {
-            type: Number,
-            value: 10000,
-          },
-
-          /**
-           * String passed to serviceWorker which triggers self.skipWaiting().
-           * String will be passed in message.action.
-           */
-          updateAction: {
-            type: String,
-            value: 'skipWaiting',
-          },
-
-          /** Reference to the active worker. */
-          worker: {
-            type: Object,
-            readOnly: true,
-            value: null,
-            notify: true,
-          },
-
-        };
+        }
       }
 
       connectedCallback() {
-        super.connectedCallback();
-        const {autoReload, path, scope, updateAction} = this;
-        this.registerServiceWorker({autoReload, path, scope, updateAction});
+        const { autoReload, path, scope, updateAction } = this;
+        this.registerServiceWorker({ autoReload, path, scope, updateAction });
+      }
+
+      attributeChangedCallback(name, newVal, oldVal) {
+        switch (name) {
+          case 'path':
+            this.path = newVal;
+            this.registerServiceWorker({ path: newVal });
+            break;
+          case 'scope': this.scope = newVal; break;
+          case 'toast-duration': this.toastDuration = newVal; break;
+          case 'update-action': this.updateAction = newVal; break;
+          case 'auto-reload': this.autoReload = this.hasAttribute(name); break;
+          case 'language': this.language = newVal; break;
+        }
       }
 
       /**
@@ -125,49 +162,59 @@ if ('serviceWorker' in navigator) {
        * @protected
        */
       async openToast() {
-        if (!this.toast) {
-          await Promise.all([
-            import('../../@polymer/paper-button/paper-button.js'),
-            import('../../@polymer/paper-toast/paper-toast.js'),
-          ]);
 
-          const button = document.createElement('paper-button');
-                button.id = 'serviceWorkerToastButton';
-                button.innerHTML = this.localize('clickToUpdate');
-                button.onclick = () => window.location.reload();
-                button.style.color = 'var(--service-worker-button-color, var(--paper-yellow-400, #FFEE58))';
-          const close = document.createElement('paper-button');
-                close.id = 'serviceWorkerCloseButton';
-                close.innerHTML = this.localize('ok');
-                close.onclick = () => toast.close();
-          const toast = document.createElement('paper-toast');
-                toast.id = 'serviceWorkerToast';
-                toast.duration = this.toastDuration;
-                toast.text = this.localize('newVersion');
-                toast.appendChild(button);
-                toast.appendChild(close);
-          this.toast = toast;
+        const actionHandler = () => window.location.reload();
+        const actionText = this.localize('ok');
+        const message = `${this.localize('newVersion')} ${this.localize('clickToUpdate')}`;
+        const timeout = this.toastDuration;
+
+        if (!window.componentHandler) {
+          await loadAsync(MDL_SCRIPT_SRC)
+        }
+
+        if (!this.toast) {
+          const { html, render } = await import('lit-html/lit-html.js');
+          const temp = document.createElement('div')
+          const snackbarTemplate = html`
+            <div class="mdl-snackbar mdl-js-snackbar"
+                aria-live="assertive"
+                aria-atomic="true"
+                aria-relevant="text">
+              <div class="mdl-snackbar__text"></div>
+              <button type="button" class="mdl-snackbar__action"></button>
+            </div>`;
+
+          render(snackbarTemplate, temp)
+          snackbar  = temp.firstElementChild;
+          this.toast = snackbar;
           document.body.appendChild(toast);
         }
-        this.toast.open();
+
+        this.toast.MaterialSnackbar.showSnackbar({
+          actionHandler,
+          actionText,
+          message,
+          timeout,
+        });
       }
 
       /**
        * Registers a service worker, and prompts to update as needed
-       * @param  {Boolean} [$0.autoReload=this.autoReload]  Path to the sw script
-       * @param  {String}  [$0.path=this.path]              Path to the sw script
-       * @param  {String}  [$0.scope=this.scope]            Scope of the sw
-       * @param  {String}  [$0.updateAction=this.updateAction] action to trigger the sw update.
+       * @param  {Object} options Initialization options
+       * @param  {Boolean} [options.autoReload=this.autoReload]  Path to the sw script
+       * @param  {String}  [options.path=this.path]              Path to the sw script
+       * @param  {String}  [options.scope=this.scope]            Scope of the sw
+       * @param  {String}  [options.updateAction=this.updateAction] action to trigger the sw update.
        * @return {Promise}
        */
-      async registerServiceWorker({autoReload, path, scope, updateAction}) {
-        if (!path || !scope || !updateAction) return;
+      async registerServiceWorker({ autoReload, path, scope, updateAction: action }) {
+        if (!path || !scope || !action) return;
         let shouldToast = true;
 
         // When an update is found, if user has not yet interacted with the page,
         // reload it for them, otherwise, prompt them to reload 🍩.
         const update = (serviceWorker) => {
-          serviceWorker.postMessage({action: updateAction});
+          serviceWorker.postMessage({ action });
           shouldToast ? this.openToast() : location.reload();
         };
 
@@ -198,7 +245,7 @@ if ('serviceWorker' in navigator) {
           return reg;
         }
 
-        if (reg.active) this._setWorker(reg.active);
+        if (reg.active) this.worker = reg.active;
 
         // If there's no previous SW, quit early - this page load is fresh. 🍌
         if (!navigator.serviceWorker.controller) return 'Page fresh.';
@@ -215,5 +262,5 @@ if ('serviceWorker' in navigator) {
       }
     }
 
-    customElements.define(ServiceWorker.is, ServiceWorker);
+    customElements.define('service-worker', ServiceWorker);
   }
